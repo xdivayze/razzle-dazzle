65b84db7e6978594fc02b5eb54a9ec9df1de2477
xdivayze <xdivayze@aperturelabs.xyz>
2026-07-07 22:40:58 +0200

    added argument action skeleton

--- src/main.py
+++ src/main.py
@@ -1,22 +1,33 @@
 import argparse
+from typing import IO, cast 
+import json
 import os 
 from pathlib import Path
 
-
+CONFIG_BASE_DIR_NAME = "base_directory"
 
 def add_arguments(parser: argparse.ArgumentParser):
+    #positional args
     parser.add_argument("journal_name",nargs="?", default=None, help = "journal name")
- 
+    #true/false args
+    
+    #does stuff
     parser.add_argument("--backup", action="store_true", help="create a back up of the provided journal")
     parser.add_argument("--new-entry", action="store_true", help="insert a new entry to the journal")
-    parser.add_argument("--list-journals", action="store_true", help="lists the journals in the current base directory")
-    parser.add_argument("--base-directory", action="store_true", help="print the current base directory")
     parser.add_argument("--remove-journal", action="store_true", help="remove the specified journal and delete its contents")
+    
+    #prints stuff
     parser.add_argument("--list-backups", action="store_true", help="list the backups for journal")
     parser.add_argument("--list-entries", action="store_true", help="list entries for journal")
+    parser.add_argument("--list-journals", action="store_true", help="lists the journals in the current base directory")
+    parser.add_argument("--print-base-directory", action="store_true", help="print the current base directory")
 
+    #field args
+    #no journal name required
     parser.add_argument("--new", type=str,metavar="JOURNAL NAME", help="create a new journal" )
     parser.add_argument("--change-base-directory",metavar="DIRECTORY", type=str, help="change the base directory")
+    
+    #journal name required
     parser.add_argument("--remove-entry", metavar="ENTRY IDENTIFIER", type=str, help="remove the specified entry from the chosen journal")
     parser.add_argument("--revert-to-backup", metavar="BACKUP IDENTIFIER", type=str, help="revert to specified backup")
 
@@ -35,8 +46,43 @@ def new_journal(dir: Path, journal_name: str):
     os.mkdir(dir/"backups")
 
 
-def argument_handler(args)->str:
-    pass
+def argument_handler( args, config_handle: IO[str], config: dict[str, object]| None ):
+    if config is None:
+            config = cast(dict[str, object],json.load(config_handle))
+    
+    ndir = Path(args.change_base_directory).resolve()
+    if ndir:
+        os.mkdir(ndir)
+        config[CONFIG_BASE_DIR_NAME] = ndir
+    
+    base_dir = config[CONFIG_BASE_DIR_NAME]
+    
+    if args.print_base_directory:
+        print(f"BASE DIRECTORY: {base_dir}")
+
+    jname = args.journal_name
+    if jname:
+        if args.revert_to_backup:
+            pass
+        if args.remove_entry:
+            pass
+        if args.backup:
+            pass
+        if args.new_entry:
+            pass
+        if args.remove_journal:
+            pass
+        if args.list_backups:
+            pass
+        if args.list_entries:
+            pass
+        if args.list_journals:
+            pass
+    if args.new:
+        pass
+
+    json.dump(config, config_handle)
+    return str(base_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -44,8 +90,31 @@ if __name__ == "__main__":
     add_arguments(parser)    
     args = parser.parse_args()
     
-    base_dir:str = argument_handler(args)
-        
+    fdir = Path(__file__).resolve().parent
+    config_path = fdir/"config.json"
+    fallback_base_dir = config_path/".data"
+
+    config = None
+    with open(config_path, "w") as f:
+        if not config_path.exists():
+            f.write(f"""
+                    {{
+                        {CONFIG_BASE_DIR_NAME}: {fallback_base_dir},
+                    }}
+                    """)
+            os.mkdir(fallback_base_dir)
+        else:
+            config = json.load(f)
+            if not config[CONFIG_BASE_DIR_NAME]:
+                f.write(f"""
+                    {{
+                        {CONFIG_BASE_DIR_NAME}: {fallback_base_dir},
+                    }}
+                    """)
+                os.mkdir(fallback_base_dir)   
+    
+    base_dir:str = argument_handler(args, f, config)
+    f.close()
 
     
 
